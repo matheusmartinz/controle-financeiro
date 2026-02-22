460f1e66954a863fdf0b1466792e3658eaca881e
matheusmartinz <matheusmartins871@gmail.com>
2026-02-22 14:30:16 -0300

    Mudança de Categoria descrição para nome - Teste de integração CategoriaRestController

--- src/main/java/com/mfinancas/api/controller/CategoriaRestController.java
+++ src/main/java/com/mfinancas/api/controller/CategoriaRestController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -17,6 +18,11 @@ public class CategoriaRestController {
     @PostMapping("/create/{uuidUsuario}")
     public ResponseEntity<CategoriaTO> createCategoria(@RequestBody CategoriaTO categoriaTO, @PathVariable UUID uuidUsuario) {
         return ResponseEntity.ok(categoriaService.createCategoria(categoriaTO, uuidUsuario));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CategoriaTO>> findAllCategoria() {
+        return ResponseEntity.ok(categoriaService.getAllCategorias());
     }
 
 }
